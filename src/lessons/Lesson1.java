c990e2d1374a0ad30650667ba8dfdc109d989a07
zhurovev <zhurovev@yandex.ru>
2024-01-03 21:16:13 +0300

    Пробная версия с нового компа

---
+++ src/lessons/Lesson1.java
@@ -0,0 +1,14 @@
+package lessons;
+
+import java.util.Arrays;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        int[] array = {5, 7, 3, 1, 2, 6, 4, 9, 0, 8};
+
+        Arrays.sort(array);
+
+        System.out.println(Arrays.toString(array));
+    }
+}
+
